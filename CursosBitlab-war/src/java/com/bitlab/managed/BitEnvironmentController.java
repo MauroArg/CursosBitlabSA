9647ba6da452c6982381f68eb4e42bf83e2b99d9
MauricioArgu <Mauro66Arg@gmail.com>
2020-08-21 16:24:10 -0600

    ManagedBeans

--- CursosBitlab-war/src/java/com/bitlab/managed/BitEnvironmentController.java
+++ CursosBitlab-war/src/java/com/bitlab/managed/BitEnvironmentController.java
@@ -14,13 +14,15 @@ import javax.ejb.EJB;
 import javax.ejb.EJBException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-@Named("bitEnvironmentController")
-@SessionScoped
+@ManagedBean
+@ViewScoped
 public class BitEnvironmentController implements Serializable {
 
     @EJB
