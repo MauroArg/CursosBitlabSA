317a9bb881963cdd759467022819813d5fafdc6f
Arivera11 <arivera@4apps.com.co>
2020-09-03 20:22:51 -0600

    Cambios en uploadFile

--- CursosBitlab-war/src/java/com/bitlab/util/UploadFile.java
+++ CursosBitlab-war/src/java/com/bitlab/util/UploadFile.java
@@ -33,7 +33,7 @@ public class UploadFile {
             if(!folder.exists()){
                 folder.mkdirs();
             }
-            
+            copyFile(archivo.getFileName(), archivo.getInputStream());
             return true;
         }catch(Exception e){
             e.printStackTrace();
